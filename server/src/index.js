55a403211f832de5d16f27233dba83f884ce60f5
JIngyi-Liu03 <19808630452@163.com>
2026-08-12 15:13:34 +0800

    reduce js/css cache to 10s

--- server/src/index.js
+++ server/src/index.js
@@ -64,7 +64,7 @@ function attachStatic(app, dir) {
             res.setHeader('Pragma', 'no-cache')
             res.setHeader('Expires', '0')
           } else {
-            res.setHeader('Cache-Control', 'public, max-age=3600')
+            res.setHeader('Cache-Control', 'public, max-age=10')
           }
         }
       })
